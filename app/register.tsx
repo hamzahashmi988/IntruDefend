977f3e443b8afeaa1cb72429605fb63c703678b6
MUHAMMAD HAMZA HASHMI <110339983+hamzahashmi988@users.noreply.github.com>
2025-01-12 23:55:28 +0500

    password

--- app/register.tsx
+++ app/register.tsx
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         fontSize: 16,
         backgroundColor: '#fff',
+        color: "black"
     },
     button: {
         height: 50,
